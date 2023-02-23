03a3ff8399671c430985a8bb36db6ad1f960250d
Reyhane Masumi <r.masumi94@gmail.com>
2023-02-23 11:29:38 +0330

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      primary: '#27AE60',
+      white: '#FFFFFF',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
